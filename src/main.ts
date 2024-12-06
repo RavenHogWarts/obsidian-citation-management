053095bb415d4d1fdb74af1161ea61304ea718e9
RavenHogWarts <fengyj120@163.com>
2024-12-06 23:49:39 +0800

    feat: Initial release of Citation Management plugin
    - Add citation insertion functionality for Obsidian notes
    - Implement citation modal for quick source selection
    - Add settings tab for managing citation sources
    - Support desktop and mobile platforms

--- src/main.ts
+++ src/main.ts
@@ -1,134 +1,165 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, Menu, TFile } from 'obsidian';
+import "../style/styles.css";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface CitationPluginSettings {
+    citations: string[];
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CitationPluginSettings = {
+    citations: []
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class CitationModal extends Modal {
+    citations: string[];
+    onSubmit: (citation: string) => void;
+    customCitation = '';
 
-	async onload() {
-		await this.loadSettings();
+    constructor(app: App, citations: string[], onSubmit: (citation: string) => void) {
+        super(app);
+        this.citations = citations;
+        this.onSubmit = onSubmit;
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    onOpen() {
+        const {contentEl} = this;
+        contentEl.empty();
+        contentEl.createEl('h2', {text: 'Insert Citation'});
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        // Predefined citations
+        if (this.citations.length > 0) {
+            contentEl.createEl('h3', {text: 'Saved Citations'});
+            const citationList = contentEl.createEl('div', {cls: 'citation-list'});
+            
+            this.citations.forEach(citation => {
+                const btn = citationList.createEl('button', {text: citation});
+                btn.onclick = () => {
+                    this.onSubmit(citation);
+                    this.close();
+                };
+            });
+        }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        // Custom citation input
+        contentEl.createEl('h3', {text: 'Custom Citation'});
+        const inputContainer = contentEl.createEl('div', {cls: 'citation-input'});
+        
+        const input = inputContainer.createEl('input', {
+            type: 'text',
+            placeholder: 'Enter custom citation'
+        });
+        input.value = this.customCitation;
+        input.oninput = (e) => {
+            this.customCitation = (e.target as HTMLInputElement).value;
+        };
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        const submitBtn = inputContainer.createEl('button', {text: 'Insert'});
+        submitBtn.onclick = () => {
+            if (this.customCitation) {
+                this.onSubmit(this.customCitation);
+                this.close();
+            }
+        };
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    onClose() {
+        const {contentEl} = this;
+        contentEl.empty();
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+export default class CitationPlugin extends Plugin {
+    settings: CitationPluginSettings;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    async onload() {
+        await this.loadSettings();
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+        // Add context menu event
+        this.registerEvent(
+            this.app.workspace.on("editor-menu", (menu: Menu, editor: Editor) => {
+                menu.addItem((item) => {
+                    item
+                        .setTitle("Insert Citation")
+                        .setIcon("quote-glyph")
+                        .onClick(() => {
+                            new CitationModal(this.app, this.settings.citations, (citation) => {
+                                const selectedText = editor.getSelection();
+                                const currentLine = editor.getCursor().line;
+                                const lineContent = editor.getLine(currentLine);
+                                
+                                if (selectedText) {
+                                    // If text is selected, append citation after selection
+                                    editor.replaceSelection(`${selectedText}[${citation}]`);
+                                } else {
+                                    // If no text is selected, append to end of current line
+                                    editor.setLine(currentLine, `${lineContent}[${citation}]`);
+                                }
+                            }).open();
+                        });
+                });
+            })
+        );
+
+        // Add settings tab
+        this.addSettingTab(new CitationSettingTab(this.app, this));
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class CitationSettingTab extends PluginSettingTab {
+    plugin: CitationPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: CitationPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    display(): void {
+        const {containerEl} = this;
+        containerEl.empty();
 
-		containerEl.empty();
+        containerEl.createEl('h2', {text: 'Citation Management Settings'});
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        // Add new citation
+        new Setting(containerEl)
+            .setName('Add New Citation')
+            .setDesc('Add a new citation to the list')
+            .addText(text => text
+                .setPlaceholder('Enter citation')
+                .then(textComponent => {
+                    const button = new Setting(containerEl)
+                        .addButton(btn => btn
+                            .setButtonText('Add')
+                            .onClick(async () => {
+                                const value = textComponent.getValue();
+                                if (value) {
+                                    this.plugin.settings.citations.push(value);
+                                    await this.plugin.saveSettings();
+                                    textComponent.setValue('');
+                                    this.display();
+                                }
+                            }));
+                }));
+
+        // List existing citations
+        containerEl.createEl('h3', {text: 'Saved Citations'});
+        
+        this.plugin.settings.citations.forEach((citation, index) => {
+            new Setting(containerEl)
+                .setName(citation)
+                .addButton(btn => btn
+                    .setButtonText('Delete')
+                    .onClick(async () => {
+                        this.plugin.settings.citations.splice(index, 1);
+                        await this.plugin.saveSettings();
+                        this.display();
+                    }));
+        });
+    }
 }
